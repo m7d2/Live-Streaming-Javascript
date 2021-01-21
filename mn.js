e93d0213f75347ae96e50c9c9b5e2d279c2b14b2
Mohammad Rashed <mo2002mohammad@gmail.com>
2021-01-21 02:48:35 -0400

    Live stream testing

---
+++ mn.js
@@ -0,0 +1,18 @@
+function Stream(){
+    const mediaStreamY = new MediaStream;
+    const videoElement = document.querySelector('video');
+    var front = false;
+    document.getElementById('flip-camera').onclick = function() { front = !front; console.log(front) };
+   let nav = navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+    }).then(mediaStreamY => {
+        videoElement.srcObject = mediaStreamY;
+        videoElement.onloadedmetadata = function(e){
+            videoElement.play();
+        }
+    });
+   
+}
+
+Stream();
